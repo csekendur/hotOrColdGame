eeca81b2c68326dc50778863239624a8727e9e69
csekendur <csekendur@gmail.com>
2013-12-12 17:10:05 +0100

    First draft

---
+++ application.js
@@ -0,0 +1,17 @@
+var answer = Math.floor((Math.random()*100)+1);
+var guess = 
+
+$.trim(guess)
+
+if (guess == NaN) {
+	return "Please enter a number!";
+}
+if else (guess) {
+
+}
+if else(guess > answer + |10|) {
+	return "cold";
+}
+if else (guess < answer + |10|) {
+	return "hot";
+}
